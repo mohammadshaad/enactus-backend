fdf41d04ce89710c54c557b7e1e3160940fde8b9
Mohammad Shaad <mohammadshaadshaikh@gmail.com>
2023-10-27 01:21:33 +0530

    :white_check_mark: Added razorpay payment system :pencil:

--- index.js
+++ index.js
@@ -1,41 +1,58 @@
+// const express = require("express");
+// const app = express();
+// require("dotenv").config();
+// const stripe = require("stripe")(process.env.STRIPE_SECRET_TEST);
+// const bodyParser = require("body-parser");
+// const cors = require("cors");
+
+// app.use(bodyParser.urlencoded({ extended: true }));
+// app.use(bodyParser.json());
+
+// app.use(cors());
+
+// app.post("/payment", cors(), async (req, res) => {
+//   let { amount, id } = req.body;
+//   try {
+//     const payment = await stripe.paymentIntents.create({
+//       amount,
+//       currency: "INR",
+//       description: "Enactus VITC",
+//       payment_method: id,
+//       //   confirm: true,
+//       //   confirmParams: { return_url: "http://localhost:3000/confirm" },
+//     });
+
+//     console.log("Payment", payment);
+//     res.json({
+//       message: "Payment successful",
+//       success: true,
+//     });
+//   } catch (error) {
+//     console.log("Error", error);
+//     res.json({
+//       message: "Payment failed",
+//       success: false,
+//     });
+//   }
+// });
+
+// app.listen(process.env.PORT || 8000, () => {
+//   console.log(`Server is listening on port ${process.env.PORT || 8000}`);
+// });
+
 const express = require("express");
+
 const app = express();
-require("dotenv").config();
-const stripe = require("stripe")(process.env.STRIPE_SECRET_TEST);
-const bodyParser = require("body-parser");
-const cors = require("cors");
+const port = process.env.PORT || 8080;
 
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json());
-
+var cors = require('cors');
 app.use(cors());
 
-app.post("/payment", cors(), async (req, res) => {
-  let { amount, id } = req.body;
-  try {
-    const payment = await stripe.paymentIntents.create({
-      amount,
-      currency: "INR",
-      description: "Enactus VITC",
-      payment_method: id,
-      //   confirm: true,
-      //   confirmParams: { return_url: "http://localhost:3000/confirm" },
-    });
 
-    console.log("Payment", payment);
-    res.json({
-      message: "Payment successful",
-      success: true,
-    });
-  } catch (error) {
-    console.log("Error", error);
-    res.json({
-      message: "Payment failed",
-      success: false,
-    });
-  }
-});
+// middlewares
+app.use(express.json({ extended: false }));
 
-app.listen(process.env.PORT || 8000, () => {
-  console.log(`Server is listening on port ${process.env.PORT || 8000}`);
-});
+// route included
+app.use("/payment", require("./payment"));
+
+app.listen(port, () => console.log(`server started on port ${port}`));
