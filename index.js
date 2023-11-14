82f94c8bfd6df64057e6412747a53b58cde63cf8
Mohammad Shaad <mohammadshaadshaikh@gmail.com>
2023-11-14 23:50:14 +0530

    :fire: Added pay id in the success page :tada:

--- index.js
+++ index.js
@@ -36,7 +36,7 @@
 
 const express = require('express');
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 8080;
 
 // Enable CORS for all routes
 app.use((req, res, next) => {
