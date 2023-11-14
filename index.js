74f799105333b742e34928954d3085e774fcf672
Mohammad Shaad <mohammadshaadshaikh@gmail.com>
2023-11-15 00:07:32 +0530

    :fire: Added pay id in the success page :tada:

--- index.js
+++ index.js
@@ -33,24 +33,26 @@
 // app.use("/payment", require("./payment"));
 
 // app.listen(port, () => console.log(`server started on port ${port}`));
- 
 
 // ---------------------------------------------------------------------------------------------
 
-const express = require('express');
+const express = require("express");
 const app = express();
 const port = process.env.PORT || 8080;
 
 // Enable CORS for all routes
 app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', 'https://www.enactusvitc.com'); // Specify the allowed origin
-  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS'); // Specify the allowed HTTP methods
-  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization'); // Specify the allowed headers
-  res.header('Access-Control-Allow-Credentials', true); // Allow sending cookies from the origin
-  res.header('Access-Control-Max-Age', '86400'); // Cache the preflight request for 24 hours
+  if (req.method === "OPTIONS") {
+    res.header("Access-Control-Allow-Origin", "https://www.enactusvitc.com"); // Specify the allowed origin
+    res.header(
+      "Access-Control-Allow-Methods",
+      "GET, POST, PUT, DELETE, OPTIONS"
+    ); // Specify the allowed HTTP methods
+    res.header("Access-Control-Allow-Headers", "Content-Type, Authorization"); // Specify the allowed headers
+    res.header("Access-Control-Allow-Credentials", true); // Allow sending cookies from the origin
+    res.header("Access-Control-Max-Age", "86400"); // Cache the preflight request for 24 hours
 
-  // Handle preflight requests
-  if (req.method === 'OPTIONS') {
+    // Handle preflight requests
     res.sendStatus(200);
   } else {
     next();
